11a603b32ed6994e3e469d71444926f10df61836
aminajaouani1 <amina.jaouani@omnilog.fr>
2025-06-04 14:31:12 +0200

    Managing comments, feedback page, unabling comments and photots to be added as well as displayed

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -58,9 +58,7 @@ class CommentCrudController extends AbstractCrudController
         ]);
         if (Crud::PAGE_EDIT === $pageName) {
             yield $createdAt->setFormTypeOption('disabled', true);
-        } else {
-            yield $createdAt;
-        }
+        } 
     }
     
 }
